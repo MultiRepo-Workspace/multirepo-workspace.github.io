f4378723454f29c5510b6f0a212cf9511f389647
Juan Ezquerro LLanes <juan@syncrtc.com>
2020-06-24 00:32:53 +0200

    pages

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="MultiRepo-Workspace logo" src="./assets/logo.png">
-    <Landing msg="MultiRepo Workspace"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="MultiRepo Workspace Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+      <v-toolbar-title>MultiRepo Workspace</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/MultiRepo-Workspace/cli/releases"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <Landing/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Landing from './components/Landing.vue'
+import Landing from './components/Landing';
 
 export default {
   name: 'App',
-  components: {
-    Landing
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Landing,
+  },
+  created () {
+      this.$vuetify.theme.dark = true
+    },
+};
+</script>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,32 +1,49 @@
 <template>
-  <div class="landing">
-    <h1>{{ msg }}</h1>
-  </div>
+  <v-container>
+    <v-card
+      class="mx-auto"
+      max-width=800
+      >
+      <v-card-title>Open workspace UI</v-card-title>
+      <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+      <v-card-text class="text--primary">
+      <v-img
+        class="align-end"
+        src="../assets/workspace-ui.png"
+        >
+      </v-img>
+      <div>Whitehaven Beach</div>
+      <div>Whitsunday Island, Whitsunday Islands</div>
+      <v-img
+        class="align-end"
+        src="../assets/workspace-ui-html.png"
+        >
+      </v-img>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'Landing',
-  props: {
-    msg: String
+  export default {
+    name: 'Landing',
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+margin: 40px 0 0;
 }
 ul {
   list-style-type: none;
-  padding: 0;
+padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+display: inline-block;
+margin: 0 10px;
 }
 a {
-  color: #42b983;
+color: #42b983;
 }
 </style>
